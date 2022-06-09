c5b48db45bb3189f131492db35a3452f15f88ab8
K. Yasin <58375357+kYasinAblay@users.noreply.github.com>
2022-06-09 10:12:00 +0300

    Add project files.

---
+++ TaskProgramming/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TaskProgramming
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ WaitingForTimeToPass/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WaitingForTimeToPass
+    {
+        class WaitingForTimeToPass
+        {
+            static void Main(string[] args)
+            {
+                // we've already seen the classic Thread.Sleep
+
+                var cts = new CancellationTokenSource();
+                var token = cts.Token;
+                var t = new Task(() =>
+                {
+                    Console.WriteLine("You have 5 seconds to disarm this bomb by pressing a key");
+                    bool canceled = token.WaitHandle.WaitOne(5000);
+                    Console.WriteLine(canceled ? "Bomb disarmed." : "BOOM!!!!");
+                }, token);
+                t.Start();
+
+                // unlike sleep and waitone
+                // thread does not give up its turn
+                // avoiding a context switch
+                Thread.SpinWait(10000);
+                SpinWait.SpinUntil(() => false);
+                Console.WriteLine("Are you still here?");
+
+                Console.ReadKey();
+                cts.Cancel();
+
+                Console.WriteLine("Main program done, press any key.");
+                Console.ReadKey();
+            }
+        }
+    }
