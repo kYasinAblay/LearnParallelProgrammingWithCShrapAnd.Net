0957cc0d4dbed357ee38b43f0f70317ae1a09f10
K. Yasin <58375357+kYasinAblay@users.noreply.github.com>
2022-06-16 10:58:02 +0300

    okey

---
+++ AsyncFactoryMethod/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace AsyncFactoryMethod
+{
+    class Foo
+    {
+        private Foo()
+        {
+
+        }
+        private async Task<Foo> InitAsync()
+        {
+            await Task.Delay(1000);
+            return this;
+        }
+        public static Task<Foo> CreateAsync()
+        {
+            var result = new Foo();
+            return result.InitAsync();
+        }
+    }
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Foo x = await Foo.CreateAsync();
+        }
+    }
+}
---
+++ AsynchronousInitializationPattern/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace AsynchronousInitializationPattern
+{
+    public interface IAsyncInit
+    {
+        Task InitTask { get; }
+    }
+    public class MyOtherClass : IAsyncInit
+    {
+        private readonly MyClass myClass;
+        public MyOtherClass(MyClass myClass)
+        {
+            this.myClass = myClass;
+            InitTask = InitAsync();
+        }
+        public Task InitTask { get; }
+        private async Task InitAsync()
+        {
+            if (myClass is IAsyncInit ai)
+                await ai.InitTask;
+
+            await Task.Delay(1000);
+        }
+    }
+    public class MyClass : IAsyncInit
+    {
+        public MyClass()
+        {
+            InitTask = InitAsync();
+        }
+        public Task InitTask { get; }
+        private async Task InitAsync()
+        {
+            await Task.Delay(1000);
+        }
+    }
+    internal class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            //var myClass1 = new MyClass();
+
+            //if (myClass1 is IAsyncInit ai)
+            //    await ai.InitTask;
+            //}
+
+            var myClass = new MyClass();
+            var oc = new MyOtherClass(myClass);
+            await oc.InitTask;
+            
+        }
+    }
+}
